7eea60b9dc5d65ca41df1594e6970da32b06fe5f
Thiago T. P. Silva <thiagoteodoro501@gmail.com>
2021-02-13 23:35:06 -0300

    Added test for indReduceTest

--- test/Test.hs
+++ test/Test.hs
@@ -7,8 +7,26 @@ import Symbolic.STVar
 import Symbolic.Term
 import Symbolic.Var
 
+gamma n = defaultVar{name='γ', index1=Just n}
+
+indReduceTuples = [(term1, res1), (term2, res2), (term3, res3)]
+  where term1 = foldMap toTerm [(a 0, 2 :: Int), (u 0, 2), (vik 1 0, 2)]
+        res1 = (STVar $ toTerm (vik 1 0, 2 :: Int), toExpr (a 0) ^ 2 * toExpr (gamma 2))
+        term2 = foldMap toTerm [(a 0, 1 :: Int), (a 1, 1), (u (-1), 1), (u 0, 1), (vik 1 0, 2)]
+        res2 = (STVar $ foldMap toTerm [(vik 1 0, 2 :: Int), (u (-1), 1)], 0)
+        term3 = foldMap toTerm [(a 1, 2 :: Int), (u (-1), 2), (vik 1 0, 2)]
+        res3 = (STVar $ foldMap toTerm [(u (-1), 2 :: Int), (vik 1 0, 2)], toExpr (a 1) ^ 2)
+
+
+indReduceTest :: TestTree
+indReduceTest = testGroup "indReduce" [testCase (show term) (indReduce isInd reduce term @?= res) | (term, res) <- indReduceTuples]
+
+
 kernelOutputTest :: TestTree
 kernelOutputTest = testGroup "Output" [testCase "1 1" (runModel 1 1 @?= out11)]
   where out11 = KernelOutput{numberOfEqs=1,numberOfLevels=1,levelSize=[1]}
 
-main = defaultMain kernelOutputTest
+allTests :: TestTree
+allTests = testGroup "Test" [indReduceTest, kernelOutputTest]
+
+main = defaultMain allTests
