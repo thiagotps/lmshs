ce53597acd28322d601043af72eb6936151e1fc4
Thiago T. P. Silva <thiagoteodoro501@gmail.com>
2021-02-19 12:49:30 -0300

    Beginning with a simple hspec test

--- test/Test.hs
+++ test/Test.hs
@@ -1,32 +1,17 @@
 module Main where
-import Test.Tasty
-import Test.Tasty.HUnit
+import Test.QuickCheck
+import Test.Hspec
 import Model.Classic
-import Symbolic.Expr
 import Symbolic.STVar
-import Symbolic.Term
-import Symbolic.Var
 
-gamma n = defaultVar{name='γ', index1=Just n}
+testNumberOfEquations = hspec $ do
+  describe "Number Of Equations"  $ do
+    testNumber 1 1 1
+    testNumber 1 3 19
+    testNumber 2 3 394
+    testNumber 3 2 698
+  where testNumber n m res =
+          it ("N = " ++ show n ++ " and M = " ++ show m ++ " should produce " ++ show res) $ do
+            numberOfEqs (runModel n m) `shouldBe` res
 
-indReduceTuples = [(term1, res1), (term2, res2), (term3, res3)]
-  where term1 = foldMap toTerm [(a 0, 2 :: Int), (u 0, 2), (vik 1 0, 2)]
-        res1 = (STVar $ toTerm (vik 1 0, 2 :: Int), toExpr (a 0) ^ 2 * toExpr (gamma 2))
-        term2 = foldMap toTerm [(a 0, 1 :: Int), (a 1, 1), (u (-1), 1), (u 0, 1), (vik 1 0, 2)]
-        res2 = (STVar $ foldMap toTerm [(vik 1 0, 2 :: Int), (u (-1), 1)], 0)
-        term3 = foldMap toTerm [(a 1, 2 :: Int), (u (-1), 2), (vik 1 0, 2)]
-        res3 = (STVar $ foldMap toTerm [(u (-1), 2 :: Int), (vik 1 0, 2)], toExpr (a 1) ^ 2)
-
-
-indReduceTest :: TestTree
-indReduceTest = testGroup "indReduce" [testCase (show term) (indReduce isInd reduce term @?= res) | (term, res) <- indReduceTuples]
-
-
-kernelOutputTest :: TestTree
-kernelOutputTest = testGroup "Output" [testCase "1 1" (runModel 1 1 @?= out11)]
-  where out11 = KernelOutput{numberOfEqs=1,numberOfLevels=1,levelSize=[1]}
-
-allTests :: TestTree
-allTests = testGroup "Test" [indReduceTest, kernelOutputTest]
-
-main = defaultMain allTests
+main = testNumberOfEquations
