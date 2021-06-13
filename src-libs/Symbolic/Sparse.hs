fd5092d1ab5f8e689339177c47c1186759102292
Thiago T. P. Silva <thiagoteodoro501@gmail.com>
2021-06-13 13:42:24 -0300

    PowerMethod

--- src-libs/Symbolic/Sparse.hs
+++ src-libs/Symbolic/Sparse.hs
@@ -1,4 +1,4 @@
--- |
+-- | TODO: Write tests for this module
 module Symbolic.Sparse (module Symbolic.Sparse) where
 
 import Control.Monad
@@ -6,10 +6,12 @@ import Data.Vector.Generic.Mutable (new, write)
 import qualified Data.Vector.Generic.Mutable as V
 import Data.Vector.Unboxed (create, (!))
 import qualified Data.Vector.Unboxed as V
+import qualified Data.Vector.Unboxed as Vb
+import qualified Data.List as L
 
 type Vector = V.Vector Double
 
-newtype Sparse = Sparse [[(Int, Double)]] deriving (Eq, Show)
+newtype Sparse = Sparse {getSparse :: [[(Int, Double)]]} deriving (Eq, Show)
 
 infix 7 !*!
 
@@ -22,6 +24,14 @@ infix 7 !*!
     write c i s
   return c
 
+norm :: Vector -> Double
+norm = V.sum . V.map (^2)
+
+normalizeVec :: Vector -> Vector
+normalizeVec v = V.map (/l) v
+  where
+    l = norm v
+
 infixl 6 !+!
 
 (!+!) :: Vector -> Vector -> Vector
@@ -30,3 +40,19 @@ infixl 6 !+!
 infix 7 !.!
 
 a !.! b = V.sum $ V.zipWith (*) a b
+
+
+powerMethodIteration :: Sparse -> [Double]
+powerMethodIteration a = eigen
+  where
+    n = length . getSparse $ a
+    b0 = Vb.generate n (const 1)
+    b = b0 : [ normalizeVec bk | bkl1 <- b, let bk = a !*! bkl1]
+    eigen = [(bk !.! (a !*! bk)) / (bk !.! bk) | bk <- tail b]
+
+maxEigenValue :: Sparse -> Maybe Double
+maxEigenValue a = fmap snd . L.find (\(f,s) -> abs(s - f) <= abs f * precision) . take maxIterNumber $ zip eigenList (tail eigenList)
+  where
+    eigenList = powerMethodIteration a
+    precision = 10 ** (-5)
+    maxIterNumber = 10^4
